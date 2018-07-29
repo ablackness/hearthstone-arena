f97398b6bb5dab8d0105e4b908d58b3ec77a8b1e
Alex Black <alexblack426@gmail.com>
2018-07-29 14:49:01 -0700

    fixed issue where you could load card images multipple times

--- public/main.js
+++ public/main.js
@@ -1,32 +1,53 @@
 //const config = require('../src/config');
 var goButton = document.getElementById("go_button");
+var arenaButton = document.getElementById("arena");
 var $output = $("#output");
-//console.log(config.authHeader);
 const STANDARD = ["Basic","The Witchwood","Kobolds & Catacombs","Journey to Un'Goro","Knights of the Frozen Throne"];
+var selectedClass = "Neutral";
+var selectedSet = "Standard";
+var STATE = {
+    displayed: false
+};
+
+$('#class').change(function() {
+    selectedClass = $("#class").val();
+    STATE.displayed = false;
+})
+
+$('#set').change(function() {
+    selectedSet = $("#set").val();
+    STATE.displayed = false;
+})
 
 goButton.onclick = function(){
-    $selectedOption = $("#class").val();
-    $selectedSet = $("#set").val();
-    $.ajax({
-        type: "GET", 
-        url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/' + $selectedOption +'?collectible=1',
-        headers: {"X-Mashape-Key": "LVB7uJHeBDmshKSk9D8U9ofJx1j8p1rVxMejsnbqnrkQoyVhe5"},
-        success: function(data) {
-            $output.html("");
-            console.log(data);
-            if ($selectedSet === "Standard") {
-                data.map( result => {
-                    if (result.cardSet === STANDARD[0] || result.cardSet === STANDARD[1] || result.cardSet === STANDARD[2] || result.cardSet === STANDARD[3] || result.cardSet === STANDARD[4]){
-                        $output.append("<img src='" + result.img +"'/>");
-                    }
-                })
-            } else {
-                data.map( result => {
-                    if (result.cardSet === $selectedSet){
-                        $output.append("<img src='" + result.img +"'/>");
-                    }
-                })
+    console.log(STATE, selectedClass, selectedSet)
+    if(!STATE.displayed) {
+        STATE.displayed = true;
+        $.ajax({
+            type: "GET", 
+            url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/' + selectedClass +'?collectible=1',
+            headers: {"X-Mashape-Key": "LVB7uJHeBDmshKSk9D8U9ofJx1j8p1rVxMejsnbqnrkQoyVhe5"},
+            success: function(data) {
+                $output.html("");
+                console.log(data);
+                if (selectedSet === "Standard") {
+                    data.map( result => {
+                        if (result.cardSet === STANDARD[0] || result.cardSet === STANDARD[1] || result.cardSet === STANDARD[2] || result.cardSet === STANDARD[3] || result.cardSet === STANDARD[4]){
+                            $output.append("<img src='" + result.img +"'/>");
+                        }
+                    })
+                } else {
+                    data.map( result => {
+                        if (result.cardSet === selectedSet){
+                            $output.append("<img src='" + result.img +"'/>");
+                        }
+                    })
+                }
             }
-        }
-    });    
+        });
+    }    
+};
+
+arenaButton.onclick = function() {
+    window.location.assign('http://localhost:8080/arena');
 };
--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.send("Hello World! I've made a change to the server code....");
+})
+
+app.get('/arena', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../public/arenaTracker.html'));
 })
 module.exports = app;
